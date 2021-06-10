b0ee33031a84449b552c8e1edc2e748c3894ed5b
lamanmamed <80058318+lamanmamed@users.noreply.github.com>
2021-06-11 00:39:28 +0400

    Add files via upload

---
+++ Game_of_Life/Console/ainsi_console.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "board_drawer.h"
+
+void print(int **array, int rows, int columns)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            if (array[i][j] == 0)
+                printf(REDHB " %d ", array[i][j]);
+            else
+                printf(BLUHB " %d ", array[i][j]);
+        }
+        printf(reset "\n");
+    }
+}
---
+++ Game_of_Life/build/CMakeFiles/GameOfLife.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/user/SUBJECTS/Development Technics/Game_of_Life/main.c" "/home/user/SUBJECTS/Development Technics/Game_of_Life/build/CMakeFiles/GameOfLife.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/user/SUBJECTS/Development Technics/Game_of_Life/build/Game/CMakeFiles/Game.dir/DependInfo.cmake"
+  "/home/user/SUBJECTS/Development Technics/Game_of_Life/build/Console/CMakeFiles/Console.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
