a7906bc400ebbcc7a4bb1723cafd26c92defe733
Leyla Gasimova <77205086+LeylaG13@users.noreply.github.com>
2021-06-11 16:43:49 +0400

    Add files via upload

---
+++ Console/ainsi_console.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "board_drawer.h"
+
+void print(int **array, int rows, int columns)
+{
+    printf("\n");
+    // printf(clearscreen);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            if (array[i][j] == 0)
+                printf(WHTHB "%d ", array[i][j]);
+            else
+                printf(BLUHB "%d ", array[i][j]);
+        }
+        printf(reset "\n");
+    }
+    printf(reset "\n");
+}
---
+++ main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include "./Game/life.h"
+#include "./Console/board_drawer.h"
+
+int main()
+
+{
+    srand(time(0));
+    int rows, columns, mode;
+    puts("Enter number of columns: ");
+    scanf("%d", &columns);
+    puts("Enter number of rows: ");
+    scanf("%d", &rows);
+    puts("For mode clipped enter 1, for mode circular enter 2");
+    scanf("%d", &mode);
+
+    int *arr[rows];
+    for (int i = 0; i < rows; i++)
+        arr[i] = (int *)malloc(columns * sizeof(int));
+
+    initialize(arr, rows, columns);
+    random_alive(arr, rows, columns);
+    print(arr, rows, columns);
+
+    sleep(2);
+
+    while (1)
+    {
+        if (mode == 1)
+        {
+            clipped(arr, rows, columns);
+        }
+        else
+        {
+            circular(arr, rows, columns);
+        }
+        print(arr, rows, columns);
+        sleep(1);
+    }
+
+    for (int i = 0; i < rows; i++)
+        free(arr[i]);
+    return 0;
+}
